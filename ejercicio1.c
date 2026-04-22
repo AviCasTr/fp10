17feee3f5f43991df3a132fd5114177ba1e1d042
AviCasTr <avrilelenacastillotrejo@gmail.com>
2026-04-22 14:51:36 +0000

    ejercicio

---
+++ ejercicio1.c
@@ -0,0 +1,40 @@
+
+#inlcude <stdio.h> 
+#define TAM 1000
+
+void leer_matriz (int mat[][TAM],int m , int n ){
+for(int i =0; i < m; i++){
+for(int j=0; j <n; j++){
+    printf("Ingresa el valor para M[%d, %d]: ");
+    scanf("%d", &mat[i][j]);
+        }   
+
+    }
+
+}
+void mostrar_matriz(int mat[][TAM], int m, int n)
+{
+    for(int i = 0; i < m; i++)
+    {
+    printf("[ ");
+    for(int j = 0; j < n; j++)
+    {
+    printf("%d ", mat[i][j]);
+    }
+printf("]\n");
+}
+}
+
+
+void sumar_matrices (int matA[][TAM], int matB[][TAM], int matC[][TAM])
+for [int i=0; i<m-1; i++];
+    for[int j=0; j<n-1 j++];
+    matc[i][j]
+
+
+int main
+printf("Selecciona operaciòn: ")
+printf("1. Suma")
+printf("2. Multiplicaciòn")
+
+   
